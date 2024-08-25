0fce6621514685dd35f240e77d230699dfe7d80b
Nik Kantar <nik@nkantar.com>
2024-08-24 22:10:13 -0700

    Clean up for new project tooling
    Includes changes to accommodate uv, ruff, mypy, etc.

--- tests/test_generate_sanitize_url.py
+++ tests/test_generate_sanitize_url.py
@@ -1,14 +1,14 @@
-from src.sus import _sanitize_url
+from sus.sus import _sanitize_url
 
 
-def test_sanitize_url_nothing():
+def test_sanitize_url_nothing() -> None:
     url = "https://example.com/foo"
     sanitized_url = _sanitize_url(url)
 
     assert sanitized_url == "https://example.com/foo"
 
 
-def test_sanitize_url_single_quote():
+def test_sanitize_url_single_quote() -> None:
     url = "https://example.com/foo'bar"
     sanitized_url = _sanitize_url(url)
 
--- tests/test_sus.py
+++ tests/test_sus.py
@@ -1,37 +1,41 @@
 from pathlib import Path
-from unittest.mock import call
+from unittest.mock import Mock, call, patch
 
-from src.sus import sus
-
-
-def read_input_mock():
-    return ["foo|https://example.com/bar", "baz|https://example.com/barf"]
+from sus.sus import sus
 
 
-GENERATE_PAGE_PARAMS_MOCK_RETURNS = [
-    ["foo", "<html>bar</html>"],
-    ["baz", "<html>barf</html>"],
-]
-
-
-def test_sus(mocker):
-    mocker.patch("src.sus.OUTPUT_DIR", Path("test_dir"))
-    _ensure_dir_mock = mocker.patch("src.sus._ensure_dir")
-    _read_input_mock = mocker.patch("src.sus._read_input", side_effect=read_input_mock)
-    _generate_page_params_mock = mocker.patch(
-        "src.sus._generate_page_params", side_effect=GENERATE_PAGE_PARAMS_MOCK_RETURNS
-    )
-    _generate_page_mock = mocker.patch("src.sus._generate_page")
-
+@patch("sus.sus.OUTPUT_DIR", Path("test_dir"))
+@patch("sus.sus._ensure_dir")
+@patch(
+    "sus.sus._read_input",
+    return_value=[
+        "foo|https://example.com/bar",
+        "baz |  https://example.com/barf",  # spaces are intentional
+    ],
+)
+@patch(
+    "sus.sus._generate_page_params",
+    side_effect=[
+        ["foo", "<html>bar</html>"],
+        ["baz", "<html>barf</html>"],
+    ],
+)
+@patch("sus.sus._generate_page")
+def test_sus(
+    _generate_page_mock: Mock,
+    _generate_page_params_mock: Mock,
+    _read_input_mock: Mock,
+    _ensure_dir_mock: Mock,
+) -> None:
     sus()
 
-    _ensure_dir_mock.assert_called_once_with(Path("test_dir"))
-    _read_input_mock.assert_called_once()
-    assert _generate_page_params_mock.call_count == 2
-    _generate_page_params_mock.assert_has_calls(
-        [call("foo|https://example.com/bar"), call("baz|https://example.com/barf")]
-    )
-    assert _generate_page_mock.call_count == 2
-    _generate_page_mock.assert_has_calls(
-        [call("foo", "<html>bar</html>"), call("baz", "<html>barf</html>")]
-    )
+    assert _ensure_dir_mock.call_args_list == [call(Path("test_dir"))]
+    assert _read_input_mock.call_args_list == [call()]
+    assert _generate_page_params_mock.call_args_list == [
+        call("foo|https://example.com/bar"),
+        call("baz |  https://example.com/barf"),  # spaces are intentional
+    ]
+    assert _generate_page_mock.call_args_list == [
+        call("foo", "<html>bar</html>"),
+        call("baz", "<html>barf</html>"),  # note lack of spaces
+    ]
